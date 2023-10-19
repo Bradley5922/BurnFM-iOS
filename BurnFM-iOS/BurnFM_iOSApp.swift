bead955242e28745cb910cf5f3550eeef0bd2641
Bradley Cable <bradley5922@icloud.com>
2023-10-19 10:24:45 +0100

    Initial Commit

---
+++ BurnFM-iOS/BurnFM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnFM_iOSApp.swift
+//  BurnFM-iOS
+//
+//  Created by Bradley Cable on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BurnFM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurnFM-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BurnFM-iOS
+//
+//  Created by Bradley Cable on 19/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
